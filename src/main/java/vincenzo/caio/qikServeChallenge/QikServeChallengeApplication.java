df6380157c8cbb31221ff010238e37f4ee74f9b5
Caio Vincenzo <reisdima@hotmail.com>
2024-04-29 17:28:00 -0300

    First commit

---
+++ src/main/java/vincenzo/caio/qikServeChallenge/QikServeChallengeApplication.java
@@ -0,0 +1,13 @@
+package vincenzo.caio.qikServeChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QikServeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QikServeChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vincenzo/caio/qikServeChallenge/QikServeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package vincenzo.caio.qikServeChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QikServeChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
